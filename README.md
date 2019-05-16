d9bd448c02768572fa136c83a3a14f000cfb28c7
miirinjej <miirinjej@gmail.com>
2019-05-16 16:07:42 +0500

    Create new project via nuxt template

---
+++ README.md
@@ -0,0 +1,28 @@
+# multi-purpose
+
+A web app developed from [unity](https://www.behance.net/gallery/40611569/Honor-Unity-Free-PSD) free 
+template designed by [PixelZeesh](https://www.behance.net/pixelzeesh) lead interactive designer © 2016, available via license: [CC BY-NC-ND 4.0](https://creativecommons.org/licenses/by-nc-nd/4.0/deed.en_US) 
+
+## Preview
+
+[View live preview](https://miirinjej.github.io/multi-purpose/)
+
+## Build Setup
+
+``` bash
+# install dependencies
+$ npm install
+
+# serve with hot reload at localhost:3000
+$ npm run dev
+
+# build for production and launch server
+$ npm run build
+$ npm start
+
+# generate static project
+$ npm run generate
+```
+
+For detailed explanation on how things work, checkout [Nuxt.js docs](https://nuxtjs.org).
+
---
+++ nuxt.config.js
@@ -0,0 +1,67 @@
+import pkg from './package';
+
+export default {
+  mode: 'universal',
+
+  /*
+  ** Headers of the page
+  */
+  head: {
+    htmlAttrs: {
+      lang: 'en-US',
+    },
+    title: pkg.name,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description },
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+    ],
+  },
+
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#ffffff' },
+
+  /*
+  ** Global CSS
+  */
+  css: [
+  ],
+
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc:https://github.com/nuxt-community/modules/tree/master/packages/bulma
+    '@nuxtjs/bulma',
+    '@nuxtjs/pwa',
+  ],
+
+  /*
+  ** Build configuration
+  */
+  build: {
+    postcss: {
+      preset: {
+        features: {
+          customProperties: false,
+        },
+      },
+    },
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+    },
+  },
+};
