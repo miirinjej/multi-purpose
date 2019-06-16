42d0da9858eee843bd8cf6e6739287adbf322aa8
miirinjej <miirinjej@gmail.com>
2019-06-17 01:33:18 +0500

    Create dummy pages

---
+++ components/Dummy.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="dummy">
+    <div class="dummy__title">
+      <span
+        v-for="n in pageTitle.length"
+        class="js-fade"
+      >
+        {{ pageTitle[n - 1] }}
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Dummy',
+    data() {
+      return {
+        pageTitle: '',
+      };
+    },
+    mounted() {
+      this.getPageTitle();
+    },
+    updated() {
+      const fadeElements = document.querySelectorAll('.js-fade');
+
+      fadeElements.forEach(el => el.classList.add('is-unseen'));
+
+
+      this
+        .$anime
+        .timeline()
+        .add({
+          targets: '.js-fade',
+          opacity: 1,
+          easing: 'linear',
+          duration: 1000,
+          delay: this.$anime.stagger(300),
+          complete() {
+            fadeElements.forEach(el => el.classList.remove('is-unseen'));
+          },
+        });
+    },
+    methods: {
+      getPageTitle() {
+        this.pageTitle = $nuxt.$route.name;
+      },
+    },
+  };
+</script>
+
+<style lang="scss" scoped>
+  .dummy {
+    position: relative;
+    height: 100vh;
+    color: $color-name--white;
+    background: $color-name--cod-gray;
+  }
+
+  .dummy__title {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    font-size: calculate-rem(30);
+    transform: translate(-50%, -50%);
+  }
+
+  .is-unseen {
+    opacity: 0;
+  }
+</style>
---
+++ pages/services/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <dummy />
+  </div>
+</template>
+
+<script>
+  import Dummy from '~/components/Dummy';
+
+  export default {
+    components: {
+      Dummy,
+    },
+  };
+</script>
+
+<style>
+</style>
